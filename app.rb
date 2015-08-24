b4d1267a5384605265ff38dbf4a35aae3aa7e8a7
lindsay culver & alisa colon <student@epicodus.com>
2015-08-24 16:56:27 -0700

    add working update and delete methods.

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ require("pg")
 
 
 get("/") do
+  @tasks = Task.not_done()
   erb(:index)
 end
 
@@ -17,8 +18,7 @@ end
 
 post("/lists") do
   name = params.fetch("name")
-  list = List.new({:name => name, :id => nil})
-  list.save()
+  list = List.create({:name => name})
   erb(:success)
 end
 
@@ -36,20 +36,45 @@ post("/tasks") do
   description = params.fetch("description")
   list_id = params.fetch("list_id").to_i()
   @list = List.find(list_id)
-  @task = Task.new({:description => description, :list_id => list_id})
-  @task.save()
+  @task = Task.create({:description => description, :list_id => list_id})
   erb(:success)
 end
 
-get('/tasks/:id/edit') do
+get("/lists/:id/edit") do
+  @list = List.find(params.fetch("id").to_i())
+  erb(:list_edit)
+end
+
+delete("/lists/:id") do
+  @list = List.find(params.fetch("id").to_i())
+  @list.destroy()
+  @lists = List.all()
+  erb(:lists)
+end
+
+patch("/lists/:id") do
+  name = params.fetch("name")
+  @list = List.find(params.fetch("id").to_i())
+  @list.update({:name => name})
+  @lists = List.all()
+  erb(:lists)
+end
+
+delete("/tasks/:id") do
   @task = Task.find(params.fetch("id").to_i())
-  erb(:task_edit)
+  list_id = @task.list_id()
+  @list = List.find(list_id)
+  @task.destroy()
+  @tasks = Task.all()
+  erb(:list)
 end
 
 patch("/tasks/:id") do
   description = params.fetch("description")
   @task = Task.find(params.fetch("id").to_i())
+  list_id = @task.list_id()
+  @list = List.find(list_id)
   @task.update({:description => description})
   @tasks = Task.all()
-  erb(:index)
+  erb(:list)
 end
